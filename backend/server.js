6e972b7b3e75675afe531df57c2ed20d8e0eb0b7
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-21 15:31:53 +0800

    Appended the following to create basic express server: server.js

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
 // server.js is the entrypoint to the js server
-console.log('Hello World');
+
+// initialize expressjs: backend framework
+const express = require('express');
+// intialize dotenv: allows use of environmental variables
+const dotenv = require('dotenv').config();
+const port = 5000;
+
+// initialize app object with express methods
+const app = express();
+
+// bring up express server
+app.listen(port, () => console.log(`Server started on port ${port}`));
