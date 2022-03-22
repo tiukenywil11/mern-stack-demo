2e2270ded8f010b78f714c96f08c58853d73cd90
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-22 14:48:22 +0800

    Appended the following to organize get, post, put, delete routes: server.js, goalRoutes.js

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,31 @@
+// goalRoutes.js is where the routes for goals will be put
+
+// initialize expressjs: backend framework
+const express = require('express');
+// initialize router: express module for routing
+const router = express.Router();
+
+// create a get route
+// removed '/api/goals' from url path, because this will remain in server.js
+// changed 'app.get' to 'router.get' because router is the module being exported
+router.get('/', (req, res) => {
+    res.status(200).json({message: 'Get goals'});
+});
+
+// create a post route to create an entry
+router.post('/', (req, res) => {
+    res.status(200).json({message: 'Set goals'});
+});
+
+// create a put route to update an entry
+router.put('/:id', (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`});
+});
+
+// create a delete route to delete an entry
+router.delete('/:id', (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`});
+});
+
+// export the router module, so other files can use this.
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,15 @@ const port = process.env.PORT || 5000;
 // initialize app object with express methods
 const app = express();
 
-// create a get route
+/* Moved to routes/goalRoutes.js
+-- create a get route
 app.get('/api/goals', (req, res) => {
     res.status(200).json({message: 'Get goals'});
 });
+*/
+
+// use goalRoutes.js on server.js
+app.use('/api/goals', require('./routes/goalRoutes'));
 
 // bring up express server
 app.listen(port, () => console.log(`Server started on port ${port}`));
