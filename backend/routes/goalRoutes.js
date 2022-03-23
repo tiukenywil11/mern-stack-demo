eadffbbd31e2fd3ed679968b80bb49f878922ccf
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-23 10:56:06 +0800

    Appended the following to connect controllers to routes, and simplify routes: goalRoutes.js

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,28 +5,30 @@ const express = require('express');
 // initialize router: express module for routing
 const router = express.Router();
 // add import from goalController.js
-const { getGoals } = require('../controllers/goalController')
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
+// clean up routes with the same url
+// for '/api/goals' url, chain get and post requests
+router.route('/').get(getGoals).post(setGoal)
 
-// create a get route
-// removed '/api/goals' from url path, because this will remain in server.js
-// changed 'app.get' to 'router.get' because router is the module being exported
+// for '/api/goals/:id' url, chain put and delete requests
+router.route('/:id').delete(deleteGoal).put(updateGoal)
+
+/* Replaced with cleaner routes
+-- create a get route
+-- removed '/api/goals' from url path, because this will remain in server.js
+-- changed 'app.get' to 'router.get' because router is the module being exported
 router.get('/', getGoals);
 
-// create a post route to create an entry
-router.post('/', (req, res) => {
-    res.status(200).json({message: 'Set goals'});
-});
+-- create a post route to create an entry
+router.post('/', setGoal);
 
-// create a put route to update an entry
-router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`});
-});
+-- create a put route to update an entry
+router.put('/:id', updateGoal);
 
-// create a delete route to delete an entry
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`});
-});
+-- create a delete route to delete an entry
+router.delete('/:id', deleteGoal);
+*/
 
 // export the router module, so other files can use this.
 module.exports = router;
