d3687a09a6bd87998a6c30b3586641e096766d9b
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-24 13:52:49 +0800

    Appended the following file to add the user schema on goal schema: goalModel.js

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -4,11 +4,21 @@
 const mongoose = require('mongoose');
 
 // initialize goal object schema
-const goalSchema = mongoose.Schema({
-    text: {
-        type: String,
-        required: [true, 'Please add a text value']
-    }
+const goalSchema = mongoose.Schema(
+    {
+        // added user schema, because every user has their own sets of goals
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        }
+    },
+    {
+        text: 
+        {
+            type: String,
+            required: [true, 'Please add a text value']
+        }
     }, {
         timestamps: true,
     }
