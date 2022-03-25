4c6a825752e24f65507914dfcec73be2a9cb6635
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-25 11:28:22 +0800

    Appended the following to authenticate users: userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,7 +25,7 @@ const registerUser = asyncHandler(
         }
 
         // gets variable that returns true or false of the email exists
-        const userExists = await User.findOne({ email})
+        const userExists = await User.findOne({ email })
 
         // validates if user is already existing
         if(userExists) {
@@ -65,7 +65,25 @@ const registerUser = asyncHandler(
 // @access Public
 const loginUser = asyncHandler(
     async (req, res) => {
-        res.json({ message: 'Login User' })
+
+        // desructure the keys from req.body to get data for login validation
+        const { email, password } = req.body;
+
+        // check for user email
+        const user = await User.findOne({ email })
+
+        // validates login credentials, if they're correct
+        // bcrypt compare, compared the current password, and the hashed password
+        if(user && (await bcrypt.compare(password, user.password))) {
+            res.status(201).json({
+                _id: user.id,
+                name: user.name,
+                email: user.email
+            });
+        } else {
+            res.status(400);
+            throw new Error('Invalid credentials')
+        }
     }
 )
 
