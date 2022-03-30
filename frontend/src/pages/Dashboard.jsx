642b79554f3e246da4875392a2a6a210d6f2101d
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-30 12:33:24 +0800

    Appended the following file to restrict the dashboard, when not logged in: Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,36 @@
+// import react states
+import { useEffect } from 'react'
+// import from the react-router
+import { useNavigate } from 'react-router-dom'
+// import from react-redux
+import { useSelector } from 'react-redux'
+
+
 function Dashboard() {
+
+    // declare navigate from react-router-dom
+    const navigate = useNavigate()
+  
+    // destructure states from redux global auth state
+    const { user } = 
+    useSelector (
+      (state) => state.auth
+    )
+
+    // add useEffect function to check if anything has changed, takes two argumants
+    // first argument would be the effect function to trigger once dependecies are changed
+    // second argument would be the dependecies to check, it they have changed
+    useEffect(() => 
+    {
+      // if there is no user, navigate to login page
+      if(!user) {
+          navigate('/login')
+      }
+
+    }, 
+    [ user, navigate ]
+  )
+
   return (
     <div>Dashboard</div>
   )
